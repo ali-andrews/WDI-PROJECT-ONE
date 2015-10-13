1d67c1c16436a1f3fbe985884e7f0a8b43994e0f
ali-andrews <alexandra.andrews2010@gmail.com>
2015-10-13 17:36:45 +0100

    Made layout changes

---
+++ js/questions.js
@@ -0,0 +1,50 @@
+
+window.questions = {
+  "tech": [
+  {text: "Where was the first G.A. headquarters and when was it established?", answers:["New York, 2011", "New York, 2013", "New York, 2009", "San Francisco, 2011"], correctAnswer: 0 , questionId1: 0,},
+  {text: "How many campus's does G.A. have?", answers:["10", "8", "14", "9"], correctAnswer: 2},
+  {text: "What year was the google logo centered on its webpage?", answers:["2004", "2003", "2001", "1996"], correctAnswer: 2},
+  {text: "How much money does eBay make per second?", answers:["$340", "$100", "$90", "$290"], correctAnswer: 3},
+  {text: "How many times do users 'shazam' per day?", answers:["10,000 thousand", "10 million", "20 million", "5 million"], correctAnswer: 1},
+  {text: "What was the 2nd-most searched for topic on YouTube in 2014?", answers:["Minecraft", "Music", "Jaime Oliver", "Disney"], correctAnswer: 0},
+  {text: "How many square feet do the Amazon offices cover?", answers:["More than 700 Madison Square gardens", "Both A & B", "There ground can hold more water than 10,000 olympic pools", "5,000 rugby fields"], correctAnswer: 2},
+  {text: "How many iphones did apple sell per day in 2012", answers:["320,000", "340,000", "280,000", "180,000"], correctAnswer: 3}, 
+  ],
+  
+  "movies": [ 
+  {text: "What was the first animated movie to be nominated for an oscar?", answers:["Beauty and the Beast", "Finding Nemo", "Cinderella", "Snow white"], correctAnswer: 0},
+  {text: "What country has the biggest movie industry?", answers:["America", "United Kingdom", "India", "China"], correctAnswer: 2},
+  {text: "Only one James Bond film was approved by Chinese censors which one was it?", answers:["Golden Eye", "Quantum of Solace", "Skyfall", "Casino Royale"], correctAnswer: 3},
+  {text: "All of the clocks in Pulp Fiction show what time?", answers:["4.30", "4.20", "3", "1.30"], correctAnswer: 1},
+  {text: "What is the higest grossing film of 2013?", answers:["Hunger Games", "Frozen", "Harry Potter", "Spider Man 2"], correctAnswer: 0},
+  {text: "The original name for Mickey Mouse was suppose to be?", answers:["Mr Mouse", "Mortimer Mouse", "Mathew the Mouse", "Marcus Mouse"], correctAnswer: 1},
+  {text: "How long was Alfred Hitchcocks acceptance speech?", answers:["10 words", "50 words", "20 words", "5 words"], correctAnswer: 3},
+  {text: "Who was the highest paid female actress in 2014?", answers:["Angelina Jolie", "Jennifer Lawerence", "Julia Roberts", "Reese Witherspoon"], correctAnswer: 1},
+  ],
+
+//Which person has won the most Oscars? Walt Disney
+
+  "general": [ 
+  {text: "Who invented the snowboard?", answers:["Doctor", "Skier", "Teacher", "Engineer"], correctAnswer: 3},
+  {text: "Which is the deepest lake in the world?", answers:["Lake Superior", "Caspian Sea", "Lake Victoria", "Lake Baikal"], correctAnswer: 3},
+  {text: "What city is famous for its oranges?", answers:["Ratnagiri", "Nagpur", "Darjeeling", "Lucknow"], correctAnswer: 1},
+  {text: "How may squares are there in a Chess Board?", answers:["36", " 48", "64", "84"], correctAnswer: 2},
+  {text: "The study which deals with secret writing is known as?", answers:["Cryptography", "Secretology", "Cytography", "Cryptology"], correctAnswer: 3},
+  {text: "What band had a famous album cover featuring Battersea Power Station??", answers:["ACDC", "Pink Floyd", "Nirvana", "Queen"], correctAnswer: 2},
+  {text: "What was Elvis Presley's first UK No1 single?", answers:["Heartbreak hotel", "All Shook Up", "Love me tender", "Cant help falling in love"], correctAnswer: 2},
+  {text: "Which country has the worlds only non-rectangular flag?", answers:["Nepal", "Armenia", "Bulgaria", "Poland"], correctAnswer: 0},
+  ],
+
+
+
+
+  // <h1>Who Wants to be a Millionaire</h1>
+  // <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  //   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  //   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  //   consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  //   cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  //   proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  //   <h2>Themes</h2>
+
+}
